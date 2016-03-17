40ae3474165802749b5434b1c06297f82bfafb2d
raghunandan-gupta <raghunandan.gupta@exclusively.com>
2016-03-17 16:52:05 +0530

    class added.

---
+++ src/main/java/com/practice/services/UnWeightedShortestPathService.java
@@ -0,0 +1,10 @@
+package com.practice.services;
+
+import com.practice.model.Graph;
+
+public class UnWeightedShortestPathService<T> {
+
+	public Integer shortedPath(Graph<T> graph) {
+		return null;
+	}
+}
