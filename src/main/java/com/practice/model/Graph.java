349224a031929c56d4e4af3518060bb23333062c
raghunandan-gupta <raghunandan.gupta@exclusively.com>
2016-03-17 15:40:21 +0530

    Graph class added.

---
+++ src/main/java/com/practice/model/Graph.java
@@ -0,0 +1,61 @@
+package com.practice.model;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Graph<T> {
+
+	private Integer numOfVertex;
+	private Set<Vertex<T>> vertexSet;
+
+	private int adjacentMat[][] = null;
+	private Map<Vertex<T>, List<Vertex<T>>> adjacencyListMap;
+
+	private boolean isDirectedGraph;
+
+	public Graph(int numOfVertex, boolean isDirectedGraph) {
+		this.numOfVertex = numOfVertex;
+		this.isDirectedGraph = isDirectedGraph;
+		adjacentMat = new int[numOfVertex][numOfVertex];
+		vertexSet = new LinkedHashSet<Vertex<T>>(numOfVertex);
+	}
+
+	public void addVertex(T t) {
+		adjacencyListMap.put(new Vertex<T>(t), new ArrayList<Vertex<T>>());
+		vertexSet.add(new Vertex<T>(t));
+	}
+
+	public ArrayList<Vertex<T>> getVertexList() {
+		return new ArrayList<Vertex<T>>(this.vertexSet);
+	}
+
+	public boolean addEdge(T source, T destination, int weight) {
+		Vertex<T> sourceVertex = new Vertex<T>(source);
+		Vertex<T> destinationVertex = new Vertex<T>(destination);
+		int sourceIndex = getVertexList().indexOf(sourceVertex);
+		int destinationIndex = getVertexList().indexOf(destinationVertex);
+		adjacentMat[sourceIndex][destinationIndex] = weight;
+		adjacencyListMap.get(sourceVertex).add(destinationVertex);
+		if (!this.isDirectedGraph) {
+			adjacentMat[destinationIndex][sourceIndex] = weight;
+			adjacencyListMap.get(destinationVertex).add(sourceVertex);
+		}
+		return true;
+	}
+
+	public Integer getNumOfVertex() {
+		return this.numOfVertex;
+	}
+
+	public void displayAdjacencyMatrix() {
+		for (int i = 0; i < numOfVertex; i++) {
+			for (int j = 0; j < numOfVertex; j++) {
+				System.out.print(adjacentMat[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ src/main/java/com/practice/model/Vertex.java
@@ -0,0 +1,46 @@
+package com.practice.model;
+
+public class Vertex<T> {
+
+	T t;
+
+	public T getT() {
+		return t;
+	}
+
+	public void setT(T t) {
+		this.t = t;
+	}
+
+	public Vertex(T t) {
+		super();
+		this.t = t;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((t == null) ? 0 : t.hashCode());
+		return result;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vertex other = (Vertex) obj;
+		if (t == null) {
+			if (other.t != null)
+				return false;
+		} else if (!t.equals(other.t))
+			return false;
+		return true;
+	}
+
+}
